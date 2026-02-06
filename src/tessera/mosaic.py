6f6080117e8f0740cf05cce33f823a4f45515f67
Keerthana Kirubakaran <keerthanakirubakaran@example.com>
2026-02-06 12:03:41 +0100

    changes made

--- src/tessera/mosaic.py
+++
@@ -1,6 +0,0 @@
-from rasterio.merge import merge
-
-def create_mosaic(datasets):
-    """Merge multiple raster datasets into one mosaic"""
-    mosaic, transform = merge(datasets)
-    return mosaic, transform
