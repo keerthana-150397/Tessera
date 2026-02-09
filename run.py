f73e6a174f5b4ff0a9518d97507f7629c004637e
Keerthana Kirubakaran <keerthanakirubakaran@example.com>
2026-02-09 12:02:44 +0100

    updated

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ import os
 # -------------------------------------------------
 # CHANGE THIS PATH
 # -------------------------------------------------
-TILE_DIR = r"C:\Users\user\OneDrive - Politecnico di Milano\00TESSERA_Embeddings\2017"
+TILE_DIR = r"C:\Users\user\OneDrive - Politecnico di Milano\00TESSERA_Embeddings\2019"
 
 # Merge only RGB bands to save memory
 mosaic, transform = create_mosaic(TILE_DIR, bands=[3,2,1])
 
 # Visualize
-visualize_pca(mosaic, title="TESSERA Embeddings – Milan 2017")
+visualize_pca(mosaic, title="TESSERA Embeddings – Milan 2019")
